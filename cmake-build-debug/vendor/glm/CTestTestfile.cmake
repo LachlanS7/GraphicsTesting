2ce3a1380cae0effcb6cee9722cb9b7bda26ea55
LachlanS7 <57027406+LachlanS7@users.noreply.github.com>
2020-12-18 20:57:25 +1030

    Fix up some vendor folder and add a gitmodules file

--- cmake-build-debug/vendor/glm/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Lachlan/Desktop/dev/C++/GraphicsTesting/vendor/glm
-# Build directory: C:/Users/Lachlan/Desktop/dev/C++/GraphicsTesting/cmake-build-debug/vendor/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
--- cmake-build-debug/vendor/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Lachlan/Desktop/dev/C++/GraphicsTesting/vendor/glm/glm
-# Build directory: C:/Users/Lachlan/Desktop/dev/C++/GraphicsTesting/cmake-build-debug/vendor/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
